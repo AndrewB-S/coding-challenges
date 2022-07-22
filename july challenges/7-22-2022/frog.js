332c52a6f94f2bfc1cdb5d3847da0f0c302514e6
Andrew Sneed <ndrwsnd@gmail.com>
2022-07-22 00:52:29 -0400

    this one was dumb tbh

---
+++ july challenges/7-22-2022/frog.js
@@ -0,0 +1,9 @@
+//https://www.codewars.com/kata/57ec8bd8f670e9a47a000f89
+
+function mouthSize(animal) {
+    if(animal.toLowerCase() === 'alligator'){
+      return 'small'
+    } else {
+      return 'wide'
+    }
+  }
