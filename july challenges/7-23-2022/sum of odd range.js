997c32558fbb10cc2d496cdcd1cced0091d4f102
Andrew Sneed <ndrwsnd@gmail.com>
2022-07-24 00:36:29 -0400

    better solution

--- july challenges/7-23-2022/sum of odd range.js
+++ july challenges/7-23-2022/sum of odd range.js
@@ -1,18 +1,5 @@
 //https://www.codewars.com/kata/55fd2d567d94ac3bc9000064/train/javascript
 
 function rowSumOddNumbers(n) {
-	//n = row on the triangle structure of odd numbers ref in link
-  let startValue = (n * (n - 1) + 1)
-  let answer = []
-  if(n === 1){
-    return 1
-  } else {
-    for(i = 0; i < n; i++){
-      answer.push(startValue)
-      startValue += 2;
-    }
-    return answer.reduce((sum, value) => {
-      return value + sum
-    }, 0)
+    return Math.pow(n, 3);
   }
-}
